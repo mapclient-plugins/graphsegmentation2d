c9fdbd4d9c84aafdfec3fe23eb863bb0eec74e05
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-18 10:10:27 +1200

    Set location of step.

--- mapclientplugins/graphsegmentation2dstep/__init__.py
+++ mapclientplugins/graphsegmentation2dstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Graph Segmentation 2D'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/graphsegmentation2d/archive/v0.1.0.zip'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.graphsegmentation2dstep import step
