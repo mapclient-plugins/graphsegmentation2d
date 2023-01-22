42b813f722fe2a4bc661c12c40f1531449e07d03
TimmyNic <tim.nicolas@outlook.com>
2023-01-23 12:09:41 +1300

    Set version to 0.1.1

--- mapclientplugins/graphsegmentation2dstep/__init__.py
+++ mapclientplugins/graphsegmentation2dstep/__init__.py
@@ -2,7 +2,7 @@
 MAP Client Plugin
 '''
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Graph Segmentation 2D'
 __location__ = 'https://github.com/mapclient-plugins/graphsegmentation2d/archive/v0.1.0.zip'
